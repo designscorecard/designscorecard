968718dbbbd2831e93bb7234bda8c457b33029d9
Noah Olatoye <noaholatoye@gmail.com>
2020-04-11 22:32:24 +0100

    Static files seetings

--- designscorecard/settings.py
+++ designscorecard/settings.py
@@ -121,27 +121,17 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'main/static'),
-)
-
-# # The user-generated content is managed separately,
-# # content that is uploaded by the website users.
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'main/static'),
 )
 
 MEDIA_URL = '/media/'
